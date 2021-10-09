246ab9c8bf816583b10ec3e5d850f3698ddb418e
Maximilian Leith <maxiking0007@gmail.com>
2021-10-09 19:18:37 +0200

    added config.yaml for secrets

--- fluginfo/settings.py
+++ fluginfo/settings.py
@@ -10,20 +10,23 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-AMADEUS_KEY = 'mQXsefxpnqdSJqhqe6guFuQPBtNtDuVi'
-AMADEUS_SECRET = 'RQ7DvDAO9FfJz71Z'
-
 from pathlib import Path
+import os
+import yaml
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+CONFIG_YAML = os.path.join(BASE_DIR, 'config.yaml')
 
+# load config.yaml
+with open(CONFIG_YAML, 'r') as f:
+    config_yaml = yaml.load(f, Loader=yaml.FullLoader)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b)wgetk=5f*idwa8r5=r2-=@t-lf4(qf#trn9kz%u1zuqh3lt-'
+SECRET_KEY = config_yaml['django']['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -138,3 +141,8 @@ SPECTACULAR_SETTINGS = {
     'DESCRIPTION': 'API to query information about flights.',
     'VERSION': '0.1',
 }
+
+AMADEUS_KEY = config_yaml['amadeus']['API_KEY']
+AMADEUS_SECRET = config_yaml['amadeus']['API_SECRET']
+
+AIRHEX_KEY = config_yaml['airhex']['API_KEY']
