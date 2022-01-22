337b81688ae02a35c0e8042cfc5be1e3e3008b39
Maximilian Leith <maximilian@leith.de>
2022-01-22 18:25:50 +0100

    hide admin panel

--- fluginfo/urls.py
+++ fluginfo/urls.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+#from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
@@ -7,7 +7,7 @@ urlpatterns = [
     path('metadata/', include('metadata.urls')),
     path('availability/', include('availability.urls')),
     path('status/', include('flightstatus.urls')),
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('swagger/', SpectacularSwaggerView.as_view(), name='swagger'),
 ]
