0f79364357d74e17c7bc4379f55f9457d385819b
Maximilian Leith <maximilian@leith.de>
2022-02-08 11:55:37 +0100

    ignore secret because  database is not used anyways

--- fluginfo/settings.py
+++ fluginfo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('FLUGINFO_BACKEND_DJANGO_SECRET_KEY')
+SECRET_KEY = "not relevant"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('FLUGINFO_BACKEND_DEBUG', 'false') == 'true'
 
