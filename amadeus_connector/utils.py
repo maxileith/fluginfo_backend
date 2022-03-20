34180fb86db3bcd7b5b3f2e37a5f0c513e98a5b8
Maximilian Leith <maximilian@leith.de>
2022-03-20 15:45:34 +0100

    30 minutes max

--- amadeus_connector/utils.py
+++ amadeus_connector/utils.py
@@ -77,7 +77,7 @@ def set_cache_timeout(seconds: int):
 
 
 def timed_lru_cache(
-    _func=None, *, maxsize: int = 1024, typed: bool = False, forever: bool = False, maxseconds: int = 10e12
+    _func=None, *, maxsize: int = 1024, typed: bool = False, forever: bool = False, maxseconds: int = 1e9
 ):
     """ Extension over existing lru_cache with timeout
     :param seconds: timeout value
@@ -89,8 +89,7 @@ def timed_lru_cache(
         # create a function wrapped with traditional lru_cache
         f = lru_cache(maxsize=maxsize, typed=typed)(f)
         # convert seconds to nanoseconds to set the expiry time in nanoseconds
-        f.delta = (cache_timeout if maxseconds >
-                   cache_timeout else maxseconds) * 10 ** 9
+        f.delta = min(cache_timeout, maxseconds) * 1e9
         f.expiration = monotonic_ns() + f.delta
 
         @wraps(f)  # wraps is used to access the decorated function attributes
@@ -99,8 +98,7 @@ def timed_lru_cache(
                 # if the current cache expired of the decorated function then
                 # clear cache for that function and set a new cache value with new expiration time
                 f.cache_clear()
-                f.delta = (cache_timeout if maxseconds >
-                           cache_timeout else maxseconds) * 10 ** 9
+                f.delta = min(cache_timeout, maxseconds) * 1e9
                 f.expiration = monotonic_ns() + f.delta
             return f(*args, **kwargs)
 
